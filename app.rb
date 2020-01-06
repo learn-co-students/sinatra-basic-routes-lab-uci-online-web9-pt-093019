2860f8dfd45e5397d5520271503e7fae74a74f5e
Justin Phillips <justinphil02@yahoo.com>
2020-01-06 13:10:27 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Justin"
+  end
+
+  get '/hometown' do
+    "My hometown is Niagara Falls"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is The Boys Are Back In Town"
+  end
+
 end
