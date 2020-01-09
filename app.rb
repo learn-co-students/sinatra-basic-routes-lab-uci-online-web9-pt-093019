34cb1d252878fdf9b8a70215c7c60c78dfb1cf73
Alice Freda <alicefreda@gmail.com>
2020-01-08 18:07:42 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, world!"
+    end
+    get '/name' do
+        "My name is Alice"
+    end
+
+    get '/hometown' do
+        "My hometown is San Jose, CA"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is 5 Minutes of Funktown by Ezale"
+    end
 end
